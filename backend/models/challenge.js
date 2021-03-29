0a79bcc9d9d6d5d9d3529e4e6c3eb3a43b24e2a9
Tilak Suthar <44789847+tiks98@users.noreply.github.com>
2021-03-29 15:01:07 -0400

    Quiz page initial with working quiz and score display
    Quiz is working perfectly with displaying score at last after the quiz is done

--- backend/models/challenge.js
+++ backend/models/challenge.js
@@ -5,22 +5,7 @@ const challenge = new mongoose.Schema({
     type: String,
     required: true,
   },
-  correct_answer: {
-    type: String,
-    required: true,
-  },
-  optionOne: {
-    type: String,
-  },
-  optionTwo: {
-    type: String,
-  },
-  optionThree: {
-    type: String,
-  },
-  optionFour: {
-    type: String,
-  },
+  answers: [],
 });
 
 module.exports = mongoose.model("Challenge", challenge);
--- frontend/src/Components/IQTest.js
+++ frontend/src/Components/IQTest.js
@@ -5,14 +5,80 @@ import { AuthContext } from "../Context/AuthContext";
 
 const IQTest = (props) => {
   const { isAuthenticated, user } = useContext(AuthContext);
+  const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    Axios({
+      method: "GET",
+      url: "http://localhost:4000/getchallenge",
+    }).then((data) => {
+      console.log(data.data);
+      setQuestions(data.data);
+    });
+  }, []);
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setUserAnswers({ ...userAnswers, [e.target.name]: e.target.value });
+    console.log(userAnswers);
+  };
+
+  const answerOption = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <div>
       {!isAuthenticated ? <Redirect to="/login" /> : null}
       <h1>Welcome To IQ Test</h1>
       <h2>{!user.username ? null : user.username}</h2>
+      {showScore ? (
+        <div>
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <div>
+          {questions.length === 0 ? (
+            <h1>No Question available</h1>
+          ) : (
+            <>
+              <div className="question-section">
+                <div className="question-count">
+                  <span>Question {currentQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className="question-text">
+                  {questions[currentQuestion].question}
+                </div>
+              </div>
+              <div className="answer-section">
+                {questions[currentQuestion].answers.map((answer) => (
+                  <div>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => answerOption(answer.isCorrect)}
+                    >
+                      {answer.option}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 };
