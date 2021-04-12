40319a4a8f4825d4bca00f8a6927ea1dc177b8f3
Keval <keval8899@gmail.com>
2021-04-12 13:49:33 -0400

    ChatBox
    Here We have added chat box and group chat

--- frontend/src/Components/Messaging.js
+++ frontend/src/Components/Messaging.js
@@ -1,16 +1,26 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './ChatFeed'
+import './Chat.css'
 
-const Messaging = (props) => {
+
+const projectID = 'ba51f22d-370e-42e6-96d5-d2bcd3c3d3c5';
+
+ const Messaging = () => {
   const { isAuthenticated, user, googleLogin } = useContext(AuthContext);
+  if(!isAuthenticated){return(<Redirect to="/login" />)}
 
   return (
-    <div>
-      {!isAuthenticated ? <Redirect to="/login" /> : null}
-      <h1>Welcome to Messaging Page</h1>
-      <h2>{!user.username ? null : user.username}</h2>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={user.username}
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
 };
 
